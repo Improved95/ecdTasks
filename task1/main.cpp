3ccd58e5aeb0722808b2a21810d8060396739db5
e.davydov <e.davydov@g.nsu.ru>
2023-09-15 13:09:36 +0700

    t1

--- task1/main.cpp
+++ task1/main.cpp
@@ -1,6 +1,24 @@
 #include <iostream>
+#include<iomanip>
+#include <cmath>
+
+double fact(int N) {
+    if(N < 0) 
+        return 0;
+    if (N == 0)
+        return 1;
+    else 
+        return N * fact(N - 1);
+}
 
 using namespace std;
 int main(void) {
-    cout << "Hello world!";
+    long double expo = 0;
+    long double x = 2;
+    size_t n = 70500;
+    for (int i = 0; i < n + 1; i++) {
+        expo += (pow(x, i) / fact(i));
+    }
+
+    cout << expo << endl;
 }
