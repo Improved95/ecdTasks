1897583ef31aa5d79bb35a00a4ce81dbbedb3c3f
e.davydov <e.davydov@g.nsu.ru>
2023-09-19 19:04:44 +0700

    res for lab2

--- lab2/main.cpp
+++ lab2/main.cpp
@@ -1,20 +1,21 @@
 #include <iostream>
 #include <iomanip>
 #include <cmath>
+#include <string>
 
-double fact(long long n)
-{
-    long long f=1;
-    for(long long i=2; i<=n; i++)
-         f*=i;
+double fact(long long n) {
+    long long f = 1;
+    for(long long i=2; i<=n; i++) {
+        f *= i;
+    }
     return f;
 }
 
 using namespace std;
-int main(void) {
+int main(int argc, char* argv[]) {
     long double expo = 0;
     long double x = 2;
-    size_t n = 85000 * 3;
+    size_t n = stoull(argv[1]);
     for (int i = 0; i < n + 1; i++) {
         expo += (pow(x, i) / fact(i));
     }
---
+++ lab2/mys2.sh
@@ -0,0 +1,117 @@
+#!/bin/bash
+i1=4
+j1=4
+st=150000
+add=15000
+
+n=${st}
+echo -e "without" >> res.txt
+g++ main.cpp
+for ((i = 0; i < ${i1}; i++))
+do
+    echo "n = ${n}" >> res.txt;
+    for ((j = 0; j < ${j1}; j++)) 
+    do
+        (time ./a.out ${n}) &>> res.txt
+    done
+    n=$(( $n + $add ))
+    echo "---" >> res.txt;
+done
+
+n=${st}
+echo -e "\nO0" >> res.txt
+g++ -O0 main.cpp 
+for ((i = 0; i < ${i1}; i++))
+do
+    echo "n = ${n}" >> res.txt;
+    for ((j = 0; j < ${j1}; j++)) 
+    do
+        (time ./a.out ${n}) &>> res.txt
+    done
+    n=$(( $n + $add ))
+    echo "---" >> res.txt;
+done
+
+n=${st}
+echo -e "\nO1" >> res.txt
+g++ -O1 main.cpp 
+for ((i = 0; i < ${i1}; i++))
+do
+    echo "n = ${n}" >> res.txt;
+    for ((j = 0; j < ${j1}; j++)) 
+    do
+        (time ./a.out ${n}) &>> res.txt
+    done
+    n=$(( $n + $add ))
+    echo "---" >> res.txt;
+done
+
+n=${st}
+echo -e "\nO2" >> res.txt
+g++ -O2 main.cpp 
+for ((i = 0; i < ${i1}; i++))
+do
+    echo "n = ${n}" >> res.txt;
+    for ((j = 0; j < ${j1}; j++)) 
+    do
+        (time ./a.out ${n}) &>> res.txt
+    done
+    n=$(( $n + $add ))
+    echo "---" >> res.txt;
+done
+
+n=${st}
+echo -e "\nO3" >> res.txt
+g++ -O4 main.cpp 
+for ((i = 0; i < ${i1}; i++))
+do
+    echo "n = ${n}" >> res.txt;
+    for ((j = 0; j < ${j1}; j++)) 
+    do
+        (time ./a.out ${n}) &>> res.txt
+    done
+    n=$(( $n + $add ))
+    echo "---" >> res.txt;
+done
+
+n=${st}
+echo -e "\nOs" >> res.txt
+g++ -Os main.cpp 
+for ((i = 0; i < ${i1}; i++))
+do
+    echo "n = ${n}" >> res.txt;
+    for ((j = 0; j < ${j1}; j++)) 
+    do
+        (time ./a.out ${n}) &>> res.txt
+    done
+    n=$(( $n + $add ))
+    echo "---" >> res.txt;
+done
+
+n=${st}
+echo -e "\nOfast" >> res.txt
+g++ -Ofast main.cpp 
+for ((i = 0; i < ${i1}; i++))
+do
+    echo "n = ${n}" >> res.txt;
+    for ((j = 0; j < ${j1}; j++)) 
+    do
+        (time ./a.out ${n}) &>> res.txt
+    done
+    n=$(( $n + $add ))
+    echo "---" >> res.txt;
+done
+
+n=${st}
+echo -e "\nOg" >> res.txt
+g++ -Og main.cpp 
+for ((i = 0; i < ${i1}; i++))
+do
+    echo "n = ${n}" >> res.txt;
+    for ((j = 0; j < ${j1}; j++)) 
+    do
+        (time ./a.out ${n}) &>> res.txt
+    done
+    n=$(( $n + $add ))
+    echo "---" >> res.txt;
+done
