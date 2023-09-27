4fb2c7d66ec21b805df3a60a32b62f94e38fd9fc
e.davydov <e.davydov@g.nsu.ru>
2023-09-27 16:19:39 +0700

    time txt

--- lab2/main.cpp
+++ lab2/main.cpp
@@ -1,22 +1,32 @@
 #include <iostream>
-#include <iomanip>
-#include <cmath>
-#include <string>
+#include <ctime>
 
-double fact(long long n) {
-    long long f = 1;
-    for(long long i=2; i<=n; i++) {
-        f *= i;
-    }
-    return f;
+void swap(int &a, int &b) {
+    int c = a;
+    a = b;
+    b = c;
 }
 
 using namespace std;
 int main(int argc, char* argv[]) {
-    long double expo = 0;
-    long double x = 2;
+    srand(time(NULL));
+
     size_t n = stoull(argv[1]);
-    for (int i = 0; i < n + 1; i++) {
-        expo += (pow(x, i) / fact(i));
+    int *arr = new int[n];
+
+    for (size_t i = 0; i < n; i++) {
+        arr[i] = rand();
+    }
+
+    for (size_t i = 0; i < n - 1; i++) {
+        for (size_t j = 0; j < n - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                swap(arr[j], arr[j + 1]);
+            }
+        }
+    }
+
+    for (size_t i = 0; i < n; i++) {
+        cout << arr[i] << " ";
     }
 }
