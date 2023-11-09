3e8da81d241fc9c2a95dbda0476cd522ea1f019b
e.davydov <e.davydov@g.nsu.ru>
2023-11-10 01:05:19 +0700

    lab5

--- lab5/main.cpp
+++ lab5/main.cpp
@@ -3,11 +3,12 @@
 using namespace std;
 using namespace cv;
 
-/*int main(int argc, char *argv[]) {
+int main(int argc, char *argv[]) {
     VideoCapture capture = VideoCapture(0);
     if (!capture.isOpened()) { return 1; }
 
     Mat frame;
+    Mat grayFrame;
     double tick = 0;
     size_t tickCountSave = 0;
 
@@ -19,52 +20,37 @@ using namespace cv;
 
         size_t tickCount = getTickCount();
         tick = (tickCount - tickCountSave) / (double)getTickFrequency();
-        putText(frame, "FPS: " + to_string(1 / tick), Point(5, 20), FONT_HERSHEY_SIMPLEX, 0.5, Scalar(0, 0, 0), 1, LINE_AA);
+        putText(frame, "FPS: " + to_string((int)(1 / tick)), Point(5, 20), FONT_HERSHEY_SIMPLEX, 0.5, Scalar(0, 0, 0), 1, LINE_AA);
         tickCountSave = tickCount;
 
-        imshow("Camera", frame);
+        // Применяем черно-белый эффект
+        Mat grayFrame;
+        cvtColor(frame, grayFrame, COLOR_BGR2GRAY);
+
+        // Преобразуем в цветовое пространство HSV
+        Mat hsvFrame;
+        cvtColor(frame, hsvFrame, COLOR_BGR2HSV);
+
+        // Применяем Gaussian blur
+        Mat blurredFrame;
+        GaussianBlur(frame, blurredFrame, Size(0, 0), 3);
+
+        resize(hsvFrame, hsvFrame, grayFrame.size());
+        resize(hsvFrame, hsvFrame, grayFrame.size());
+        resize(blurredFrame, blurredFrame, grayFrame.size());
+        
+        // Mat finalFrame;
+        // addWeighted(grayFrame, 0.2, hsvFrame, 0.5, 0, finalFrame);
+        // resize(finalFrame, finalFrame, grayFrame.size());
+        // addWeighted(finalFrame, 0.7, blurredFrame, 0.3, 0, finalFrame);
+
+        imshow("Camera", blurredFrame);
         if (waitKey(30) >= 0) {
             break;
         }
     }
 
     capture.release();
-    destroyAllWindows();
-
-    return 0;
-}*/
-
-int main(int argc, char** argv) {
-    VideoCapture cap("/Users/improvedmac/Documents/RecordScreen/test.mp4");
-    if (!cap.isOpened()) {
-        cerr << "Error: could not open video file" << endl;
-        return -1;
-    }
-
-    namedWindow("Video", WINDOW_NORMAL);
-
-    Mat frame;
-    double tick = 0;
-    size_t tickCountSave = 0;
-
-    while (true) {
-        cap.read(frame);
-        if (frame.empty()) {
-            break;
-        }
-
-        size_t tickCount = getTickCount();
-        tick = (tickCount - tickCountSave) / (double)getTickFrequency();
-        putText(frame, "FPS: " + to_string((int)(1 / tick)), Point(10, 30), FONT_HERSHEY_SIMPLEX, 1, Scalar(255, 255, 255), 2, LINE_AA);
-        tickCountSave = tickCount;
-
-        imshow("Video", frame);
-        if (waitKey(30) >= 0) {
-            break;
-        }
-    }
-
-    cap.release();
     destroyAllWindows();
 
     return 0;
