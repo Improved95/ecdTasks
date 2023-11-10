dff0ddae71e02dfb245bfdeb608b45909b065c89
e.davydov <e.davydov@g.nsu.ru>
2023-11-10 15:42:43 +0700

    lab5

--- lab5/main.cpp
+++ lab5/main.cpp
@@ -21,6 +21,7 @@ int main(int argc, char *argv[]) {
         size_t tickCount = getTickCount();
         tick = (tickCount - tickCountSave) / (double)getTickFrequency();
         putText(frame, "FPS: " + to_string((int)(1 / tick)), Point(5, 20), FONT_HERSHEY_SIMPLEX, 0.5, Scalar(0, 0, 0), 1, LINE_AA);
+        putText(frame, "Time on frame: " + to_string(tick) + "s", Point(5, 35), FONT_HERSHEY_SIMPLEX, 0.5, Scalar(0, 0, 0), 1, LINE_AA);
         tickCountSave = tickCount;
 
         // Применяем черно-белый эффект
@@ -28,23 +29,23 @@ int main(int argc, char *argv[]) {
         cvtColor(frame, grayFrame, COLOR_BGR2GRAY);
 
         // Преобразуем в цветовое пространство HSV
-        Mat hsvFrame;
-        cvtColor(frame, hsvFrame, COLOR_BGR2HSV);
+        // Mat hsvFrame;
+        // cvtColor(frame, hsvFrame, COLOR_BGR2HSV);
 
         // Применяем Gaussian blur
-        Mat blurredFrame;
-        GaussianBlur(frame, blurredFrame, Size(0, 0), 3);
+        // Mat blurredFrame;
+        // GaussianBlur(frame, blurredFrame, Size(0, 0), 3);
 
-        resize(hsvFrame, hsvFrame, grayFrame.size());
-        resize(hsvFrame, hsvFrame, grayFrame.size());
-        resize(blurredFrame, blurredFrame, grayFrame.size());
+        // resize(hsvFrame, hsvFrame, grayFrame.size());
+        // resize(hsvFrame, hsvFrame, grayFrame.size());
+        // resize(blurredFrame, blurredFrame, grayFrame.size());
         
-        // Mat finalFrame;
-        // addWeighted(grayFrame, 0.2, hsvFrame, 0.5, 0, finalFrame);
+        // addWeighted(grayFrame, 0.5, hsvFrame, 0.5, 1, hsvFrame);
         // resize(finalFrame, finalFrame, grayFrame.size());
         // addWeighted(finalFrame, 0.7, blurredFrame, 0.3, 0, finalFrame);
 
-        imshow("Camera", blurredFrame);
+        imshow("Camera", grayFrame);
+        // imshow("Camera", hsvFrame);
         if (waitKey(30) >= 0) {
             break;
         }
