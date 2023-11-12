3dcfbcf5986e44db4cfe5b3ab3ec91e0163260ef
e.davydov <e.davydov@g.nsu.ru>
2023-11-12 16:46:23 +0700

    lab7

--- lab7/main.cpp
+++ lab7/main.cpp
@@ -1,7 +1,33 @@
 #include <iostream>
+#include <ctime>
+
+using namespace std;
+
+void matrixSum () {
+
+}
 
 int main() {
+    const size_t n = 10;
+    const size_t m = 10;
+    srand(100);
     
+    float **matrix = new float*[n];
+    for (size_t i = 0; i < n; i++) {
+        matrix[i] = new float[n];
+    }
+
+    for (size_t i = 0; i < n; i++) {
+        for (size_t j = 0; j < n; j++) {
+            matrix[i][j] = rand() / (float)rand();
+        }
+    }
+
     
+
+    for (size_t i = 0; i < n; i++) {
+        delete matrix[i];
+    }
+    delete[] matrix;
     return 0;
 }
