0065919618fd6a95dd448f6c6ccea1fce3fb18ff
e.davydov <e.davydov@g.nsu.ru>
2023-11-12 19:03:02 +0700

    нестатические массивы

--- lab7/main.cpp
+++ lab7/main.cpp
@@ -1,33 +1,115 @@
 #include <iostream>
 #include <ctime>
+#include <vector>
+#define N 10
+#define M 10
 
 using namespace std;
+using std::vector;
 
-void matrixSum () {
+void matrixSum (float **a, float **b) {
+    for (size_t i = 0; i < N; i++) {
+        for (size_t j = 0; j < N; j++) {
+            a[i][j] += b[i][j];
+        }
+    }
+}
 
+void matrixMul(float **a, float **b) {
+    vector<float> resV(N);
+    for (size_t i = 0; i < N; i++) {
+        for (size_t j = 0; j < N; j++) {
+            float res = 0;
+            for (size_t k = 0; k < N; k++) {
+                res += a[i][k] * b[k][j];
+            }
+            resV.push_back(res);
+        }
+        for (size_t t = 0; t < N; t++) {
+            a[i][t] = resV[t];
+        }
+    }
+}
+
+void matrixDiv(float **a, float b) {
+    for (size_t i = 0; i < N; i++) {
+        for (size_t j = 0; j < N; j++) {
+            a[i][j] /= b;
+        }
+    }
+}
+
+float maxSumRows(float **a) {
+    float maxSum = 0;
+    for (size_t i = 0; i < N; i++) {
+        float sum = 0;
+        for (size_t j = 0; j < N; j++) {
+            sum += a[i][j];
+        }
+        if (sum > maxSum) {
+            maxSum = sum;
+        }
+    }
+    return maxSum;
+}
+
+float maxSumColumns(float **a) {
+    float maxSum = 0;
+    for (size_t i = 0; i < N; i++) {
+        float sum = 0;
+        for (size_t j = 0; j < N; j++) {
+            sum += a[j][i];
+        }
+        if (sum > maxSum) {
+            maxSum = sum;
+        }
+    }
+    return maxSum;
 }
 
 int main() {
-    const size_t n = 10;
-    const size_t m = 10;
     srand(100);
     
-    float **matrix = new float*[n];
-    for (size_t i = 0; i < n; i++) {
-        matrix[i] = new float[n];
+    float **matrix = new float*[N];
+    float **matrixT = new float*[N];
+    float **matrixE = new float*[N];
+    float **matrixF = new float*[N];
+    for (size_t i = 0; i < N; i++) {
+        matrix[i] = new float[N];
+        matrixT[i] = new float[N];
+        matrixE[i] = new float[N];
+        matrixF[i] = new float[N];
     }
 
-    for (size_t i = 0; i < n; i++) {
-        for (size_t j = 0; j < n; j++) {
-            matrix[i][j] = rand() / (float)rand();
+    for (size_t i = 0; i < N; i++) {
+        for (size_t j = 0; j < N; j++) {
+            float a = rand() / (int)rand();
+            matrix[i][j] = a;
+            matrixT[j][i] = a;
+            matrixE[i][j] = 0;
+            matrixF[i][j] = 0;
         }
+        matrixE[i][i] = 1;
+    }
+    
+    matrixSum(matrixF, matrixE);
+    for (size_t i = 0; i < M - 1; i++) {
+        // matrixSum(matrixF, )
     }
 
-    
-
-    for (size_t i = 0; i < n; i++) {
+    for (size_t i = 0; i < N; i++) {
         delete matrix[i];
+        delete matrixT[i];
+        delete matrixE[i];
+        delete matrixF[i];
     }
     delete[] matrix;
     return 0;
 }
+
+/*for (size_t i = 0; i < N; i++) {
+    for (size_t j = 0; j < N; j++) {
+           cout << matrixF[i][j] << " ";
+    }
+    cout << endl;
+}*/
