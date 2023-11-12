298bedd4fb2b7a2e60a835d5a7bd389cd68205fb
e.davydov <e.davydov@g.nsu.ru>
2023-11-12 13:18:48 +0700

    lab6

--- lab6/main.cpp
+++ lab6/main.cpp
@@ -1,62 +1,10 @@
 #include <iostream>
-#include <stdio.h>
 #include <libusb.h>
+#include <stdio.h>
 
 using namespace std;
 
-void printdev(libusb_device *dev) {
-    libusb_device_descriptor desc; // дескриптор устройства
-    libusb_config_descriptor *config; // дескриптор конфигурации объекта
-    const libusb_interface *inter;
-    const libusb_interface_descriptor *interdesc;
-    const libusb_endpoint_descriptor *epdesc;
-    int r = libusb_get_device_descriptor(dev, &desc);
-
-    if (r < 0) {
-        fprintf(stderr, "Ошибка: дескриптор устройства не получен, код: %d.\n", r);
-        return;
-    }
-
-    // получить конфигурацию устройства
-    libusb_get_config_descriptor(dev, 0, &config);
-    printf("%.2d %.2d %.4d %.4d %.3d | | | | | |\n",
-           (int)desc.bNumConfigurations,
-           (int)desc.bDeviceClass,
-           desc.idVendor,
-           desc.idProduct,
-           (int)config->bNumInterfaces
-    );
-
-    for (int i = 0; i < (int)config->bNumInterfaces; i++) {
-        inter = &config->interface[i];
-        printf("| | | | | "
-               "%.2d %.2d | | | |\n",
-               inter->num_altsetting,
-               (int)desc.bDeviceClass
-        );
-
-        for (int j = 0; j < inter->num_altsetting; j++) {
-            interdesc = &inter->altsetting[j];
-            printf("| | | | | | | "
-                   "%.2d %.2d | |\n",
-                   (int)interdesc->bInterfaceNumber,
-                   (int)interdesc->bNumEndpoints
-            );
-
-            for (int k = 0; k < (int)interdesc->bNumEndpoints; k++) {
-                epdesc = &interdesc->endpoint[k];
-                printf(
-                        "| | | | | | | | | "
-                        "%.2d %.9d\n",
-                        (int)epdesc->bDescriptorType,
-                        (int)(int)epdesc->bEndpointAddress
-                );
-            }
-        }
-    }
-
-    libusb_free_config_descriptor(config);
-}
+void printdev(libusb_device *dev);
 
 int main() {
     libusb_device **devs; // указатель на указатель на устройство,
@@ -115,4 +63,58 @@ int main() {
     libusb_exit(ctx); // завершить работу с библиотекой libusb,
     // закрыть сессию работы с libusb
     return 0;
+}
+
+void printdev(libusb_device *dev) {
+    libusb_device_descriptor desc; // дескриптор устройства
+    libusb_config_descriptor *config; // дескриптор конфигурации объекта
+    const libusb_interface *inter;
+    const libusb_interface_descriptor *interdesc;
+    const libusb_endpoint_descriptor *epdesc;
+    int r = libusb_get_device_descriptor(dev, &desc);
+
+    if (r < 0) {
+        fprintf(stderr, "Ошибка: дескриптор устройства не получен, код: %d.\n", r);
+        return;
+    }
+
+    // получить конфигурацию устройства
+    libusb_get_config_descriptor(dev, 0, &config);
+    printf("%.2d %.2d %.4d %.4d %.3d | | | | | |\n",
+           (int)desc.bNumConfigurations,
+           (int)desc.bDeviceClass,
+           desc.idVendor,
+           desc.idProduct,
+           (int)config->bNumInterfaces
+    );
+
+    for (int i = 0; i < (int)config->bNumInterfaces; i++) {
+        inter = &config->interface[i];
+        printf("| | | | | "
+               "%.2d %.2d | | | |\n",
+               inter->num_altsetting,
+               (int)desc.bDeviceClass
+        );
+
+        for (int j = 0; j < inter->num_altsetting; j++) {
+            interdesc = &inter->altsetting[j];
+            printf("| | | | | | | "
+                   "%.2d %.2d | |\n",
+                   (int)interdesc->bInterfaceNumber,
+                   (int)interdesc->bNumEndpoints
+            );
+
+            for (int k = 0; k < (int)interdesc->bNumEndpoints; k++) {
+                epdesc = &interdesc->endpoint[k];
+                printf(
+                        "| | | | | | | | | "
+                        "%.2d %.9d\n",
+                        (int)epdesc->bDescriptorType,
+                        (int)(int)epdesc->bEndpointAddress
+                );
+            }
+        }
+    }
+
+    libusb_free_config_descriptor(config);
 }
