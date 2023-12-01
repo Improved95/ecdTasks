d0d054feb0d424f916fb53b47e995fcb867ce311
e.davydov <e.davydov@g.nsu.ru>
2023-12-02 00:29:32 +0700

    l8

--- lab8/main.cpp
+++ lab8/main.cpp
@@ -31,30 +31,42 @@ void backwardFill(size_t *arr, size_t len) {
     }
 }
 
-uint64_t bypass(size_t *arr, size_t len) {
-    uint64_t averageTime = 0;
+void warmingUpMemory(size_t *arr, size_t len) {
     size_t k = 0;
-
     for (size_t i = 0; i < 10; i++) {
         for (size_t j = 0; j < len; j++) {
-            uint64_t startTime = __rdtsc();
             k = arr[k];
-            uint64_t endTime = __rdtsc();
-            averageTime = (averageTime*(i + 1) + (endTime - startTime)) / (i + 2);
         }
     }
-    return averageTime;
+}
+
+uint64_t bypass(size_t *arr, size_t len) {
+    warmingUpMemory(arr, len);
+
+    // uint64_t averageTime = 0;
+    size_t k = 0;
+    uint64_t startTime = __rdtsc();
+    for (size_t i = 0; i < 10; i++) {
+        for (size_t j = 0; j < len; j++) {
+            k = arr[k];
+        }
+    }
+    
+            uint64_t endTime = __rdtsc();
+            // averageTime = (averageTime*(i + 1) + (endTime - startTime)) / (i + 2);
+            // cout << endTime << " " << startTime << endl;
+            // exit(EXIT_FAILURE);
+
+    return endTime - startTime;
+    // return averageTime;
 }
 
 void fillAndBypass(void(*fillType)(size_t *arr, size_t len)) {
-    const size_t n_min = 1024 / sizeof(size_t); //1kb
+    const size_t n_min = 1 * 1024 / sizeof(size_t); //1kb
     const size_t n_max = 32 * 1024 * 1024 / sizeof(size_t); //32mb
-    size_t step = 2 * 1024 / sizeof(size_t); //2kb
-    // size_t n_min = 1000;
-    // size_t n_max = 2000;
-    // size_t step = 100;
-    bool sf1 = true;
-    bool sf2 = true;
+    size_t step = 8 * 1024 / sizeof(size_t); //8kb
+
+    bool sf1 = true, sf2 = true, sf3 = true;
     uint64_t minTimeMemoryCalls = ULLONG_MAX;
 
     for (size_t i = n_min; i < n_max; i += step) {
@@ -71,15 +83,20 @@ void fillAndBypass(void(*fillType)(size_t *arr, size_t len)) {
 
         cout << i * sizeof(size_t) / 1024 << "kb " << minTimeMemoryCalls << " " << endl;
 
-        if (i * sizeof(size_t) > 1 * 1024 && sf1) {
-            cout << "go in 8kb" << endl; 
-            step = 8 * 1024 / sizeof(size_t);
+        if (sf1 && i * sizeof(size_t) > 256 * 1024) { // more then 256kb
+            cout << "go in 64kb" << endl; 
+            step = 64 * 1024 / sizeof(size_t);
             sf1 = false;
-        } 
-        if (i * sizeof(size_t) > 1 * 1024 * 1024  && sf2) {
-            cout << "go in 1mb" << endl; 
-            step = 1 * 1024 * 1024 / sizeof(size_t);
+        }
+        if (sf2 && i * sizeof(size_t) > 1 * 1024 * 1024) { // more then 1mb
+            cout << "go in 64kb" << endl; 
+            step = 64 * 1024 / sizeof(size_t);
             sf2 = false;
+        }
+        if (sf3 && i * sizeof(size_t) > 2 * 1024 * 1024) { // more then 2mb
+            cout << "go in 2mb" << endl; 
+            step = 2 * 1024 * 1024 / sizeof(size_t);
+            sf3 = false;
         }
 
         delete[] arr;
