f9b37c62054b8be869afa606df86c3198a4fc943
e.davydov <e.davydov@g.nsu.ru>
2023-12-01 23:57:03 +0700

    l8

--- lab8/main.cpp
+++ lab8/main.cpp
@@ -1,8 +1,7 @@
 #include <iostream>
+#include <x86intrin.h>
+#include <limits.h>
 using namespace std;
-
-// static size_t c = 0;
-// static size_t c1 = 0;
 
 void sattoloFill(size_t *a, size_t len) {
     int i, j;
@@ -32,43 +31,71 @@ void backwardFill(size_t *arr, size_t len) {
     }
 }
 
-void bypass(size_t *arr, size_t len) {
-    for (size_t i = 0, k = 0; i * 10 < len; i++) {
-        k = arr[k];
-        // c1 += 1;
+uint64_t bypass(size_t *arr, size_t len) {
+    uint64_t averageTime = 0;
+    size_t k = 0;
+
+    for (size_t i = 0; i < 10; i++) {
+        for (size_t j = 0; j < len; j++) {
+            uint64_t startTime = __rdtsc();
+            k = arr[k];
+            uint64_t endTime = __rdtsc();
+            averageTime = (averageTime*(i + 1) + (endTime - startTime)) / (i + 2);
+        }
     }
-    // c += 1;
+    return averageTime;
+}
+
+void fillAndBypass(void(*fillType)(size_t *arr, size_t len)) {
+    const size_t n_min = 1024 / sizeof(size_t); //1kb
+    const size_t n_max = 32 * 1024 * 1024 / sizeof(size_t); //32mb
+    size_t step = 2 * 1024 / sizeof(size_t); //2kb
+    // size_t n_min = 1000;
+    // size_t n_max = 2000;
+    // size_t step = 100;
+    bool sf1 = true;
+    bool sf2 = true;
+    uint64_t minTimeMemoryCalls = ULLONG_MAX;
+
+    for (size_t i = n_min; i < n_max; i += step) {
+        size_t *arr = new size_t[i];
+        if (!arr) { cout << "er" << endl; exit(EXIT_FAILURE); }
+
+        fillType(arr, i);
+        for (size_t j = 0; j < 5; j++) {
+            auto averageTimeMemoryCalls = bypass(arr, i);
+            if (averageTimeMemoryCalls < minTimeMemoryCalls) {
+                minTimeMemoryCalls = averageTimeMemoryCalls;
+            }
+        }
+
+        cout << i * sizeof(size_t) / 1024 << "kb " << minTimeMemoryCalls << " " << endl;
+
+        if (i * sizeof(size_t) > 1 * 1024 && sf1) {
+            cout << "go in 8kb" << endl; 
+            step = 8 * 1024 / sizeof(size_t);
+            sf1 = false;
+        } 
+        if (i * sizeof(size_t) > 1 * 1024 * 1024  && sf2) {
+            cout << "go in 1mb" << endl; 
+            step = 1 * 1024 * 1024 / sizeof(size_t);
+            sf2 = false;
+        }
+
+        delete[] arr;
+    }
 }
 
 int main() {
-    // size_t n_min = 2048;
-    // size_t n_max = 4718592;
-    // size_t step = 256;
 
-    // for (size_t i = n_min; i < n_max; i += step) {
-    //     size_t *arr = new size_t[i];
-    //     if (!arr) { cout << "er" << endl; return 1; }
+    cout << "forward bypass" << endl;
+    fillAndBypass(forwardFill);
 
-    //     forwardFill(arr, i);
-    //     bypass(arr, i);
+    cout << endl << "backward bypass" << endl;
+    fillAndBypass(backwardFill);
 
-    //     backwardFill(arr, i);
-    //     bypass(arr, i);
+    cout << endl << "random bypass" << endl;
+    fillAndBypass(sattoloFill);
 
-    //     sattoloFill(arr, i);
-    //     bypass(arr, i);
-
-    //     step += 256;
-    //     delete[] arr;
-    // }
-
-    size_t *arr = new size_t[20];
-    sattoloFill(arr, 20);
-    for (size_t i = 0; i < 20; i++) {
-        cout << arr[i] << " ";
-    }
-
-    // cout << c << endl;
-    // cout << c1 << endl;
     return 0;
 }
