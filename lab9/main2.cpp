7a17f1f0d37febba205f9d2052a5367040f765fd
e.davydov <e.davydov@g.nsu.ru>
2023-12-11 22:44:05 +0700

    l9

--- lab9/main2.cpp
+++ lab9/main2.cpp
@@ -1,16 +1,20 @@
 #include <iostream>
+#include <fstream>
 using namespace std;
 
+const size_t N = 100000000 * 2;
 #define NUMBER_OF_BYPASS 5
-static const size_t N = 100000000 * 2;
 
-uint64_t bypassArray(int const *array, size_t arraySize) {
+uint64_t bypassArray(unsigned int const *array) {
+    volatile size_t i;
+    volatile size_t k;
     uint64_t minTime = ULLONG_MAX;
 
-    for(size_t j = 0; j < NUMBER_OF_BYPASS; j++) {
+    for(size_t j = 0; j < NUMBER_OF_BYPASS; j++)
+    {
         volatile uint64_t startTime = __builtin_ia32_rdtsc();
 
-        for(size_t k = 0, i = 0; i < N; i++) {
+        for(k = 0, i = 0; i < N; i++) {
             k = array[k];
         }
         volatile uint64_t endTime = __builtin_ia32_rdtsc();
@@ -23,32 +27,33 @@ uint64_t bypassArray(int const *array, size_t arraySize) {
     return minTime;
 }
 
-void fillArray(int *array, int fragments, size_t offset, size_t size) {
+void initArray(unsigned int *array, unsigned int fragments, size_t offset, size_t size)
+{
     size_t i = 0;
     size_t j = 1;
 
-    for(; i < size; i++) {
-        for(; j < fragments; j++)
+    for(i = 0; i < size; i++) {
+        for(j = 1; j < fragments; j++) {
             array[i + (j - 1) * offset] = i + j * offset;
-
+        }
         array[i + (j - 1) * offset] = i + 1;
     }
 
     array[i - 1 + (j - 1) * offset] = 0;
 }
 
-void runArray(int *array, int fragments, int offset, int arraySize) {
-    fillArray(array, fragments, offset, arraySize);
-    printf("%u %llu\n", fragments, bypassArray(array, arraySize) / N);
+void runArray(unsigned int *array, unsigned int fragments, int offset, int size) {
+    initArray(array, fragments, offset, size);
+    printf("%u\t %llu\n", fragments, bypassArray(array) / N);
 }
 
 int main() {
-    // const size_t arraySize = 17 * 1024 * 1024 / sizeof(int);
-
-    auto *array = (int*) malloc(N * sizeof(int));
+    ofstream fileOut;
+    fileOut.open("time.txt");
+    auto *array = (unsigned int *) malloc(N * sizeof(unsigned int));
 
     if(!array) { return 1; }
-        
+
     for(int n = 1; n <= 32; n++) {
         runArray(array, n, 16 * 1024 * 1024 / sizeof(int), 17 * 1024 * 1024 / sizeof(int));
     }
