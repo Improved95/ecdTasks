9845b64269ec44f4522c82ac929459d28011c6a9
e.davydov <e.davydov@g.nsu.ru>
2023-12-15 13:36:04 +0700

    l10

---
+++ lab10/main2.cpp
@@ -0,0 +1,60 @@
+
+#include <iostream>
+#include <time.h>
+#include <limits.h>
+#include <fstream>
+#include <cstring>
+using namespace std;
+
+void sattoloFill(int *a, size_t len) {
+    int i, j;
+    srand(unsigned(time(0)));
+    for (size_t k = 0; k < len; k++) {
+        a[k] = k;
+    }
+    for(i = len - 1; i > 0; i--) {
+        do {
+            j = rand() % i;
+        } while(j >= i);
+        std::swap(a[i], a[j]);
+    }
+}
+
+void trashCache(int *arr, size_t len) {
+    int k = 0;
+    for (size_t i = 0; i < len; i++) {
+        k = arr[k];
+    }
+    cerr << k;
+}
+
+int main() {
+    ofstream fileOut;
+    fileOut.open("time.txt");
+
+
+    size_t trashArrSize = 17 * 1024 * 1024 / sizeof(int);
+    int *trashArr = new int[trashArrSize];
+    sattoloFill(trashArr, trashArrSize);
+
+    size_t arrSize = 128 / sizeof(int);
+    int *arr = new int[arrSize];
+    memset(arr, 0, arrSize);
+
+    int k = 0;
+    for (int step = 0; step < 32; step++) {
+            k = arr[0];
+            cerr << k;
+            volatile size_t startTime = __builtin_ia32_rdtsc();
+            k = arr[k + step];
+            volatile size_t endTime = __builtin_ia32_rdtsc();
+
+            fileOut << step << " " << endTime - startTime << endl;
+
+            cerr << k;
+            trashCache(trashArr, trashArrSize);
+    }
+    delete[] arr;
+    delete[] trashArr;
+    return 0;
+}
